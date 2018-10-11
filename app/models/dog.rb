dd0dcfe741372b9bd430571f5e22f1a89f67cfcf
Tyler <tylerthomasallen@gmail.com>
2018-10-10 18:25:54 -0700

    Add assocations for dog, types and dogtypes

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,15 @@
 class Dog < ApplicationRecord
   validates :name, :address_line_one, :zip_code, :state, :city, :lat, :lng, presence: true
   validates :name, :address_line_one, uniqueness: true
-  validates :hypoallergenic, :sheds, :good_with_kids, inclusion: { in: [:true, :false]}
+  validates :hypoallergenic, :sheds, :good_with_kids, inclusion: { in: [true, false]}
 
-  has_many :types,
+  has_many :dog_types,
     foreign_key: :dog_id,
     class_name: :DogType
+
+  has_many :types,
+    through: :dog_types,
+    source: :type
 
   after_initialize :set_defaults
 
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,9 +1,13 @@
 class Type < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many :dogs,
+  has_many :dog_types,
     foreign_key: :type_id,
     class_name: :DogType
+
+  has_many :dogs,
+    through: :dog_types,
+    source: :dog
 
   def self.find_by_name(name)
     type = Type.find_by(name: name)
