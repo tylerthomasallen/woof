8f92d832ec641ed944a8443d7e9c75cc6c748983
Tyler <tylerthomasallen@gmail.com>
2018-10-13 12:32:11 -0700

    Add opacity to background

--- frontend/components/splash/splash_header_item.jsx
+++ frontend/components/splash/splash_header_item.jsx
@@ -23,56 +23,56 @@ class SplashHeaderItem extends React.Component {
   render() {
     return (
 
-      <div className="splash-header-container">
+        <div className="splash-header-container">
 
-        <div className="splash-body">
+          <div className="splash-body">
 
-          <div className="splash-top-nav">
-            <div className="splash-top-nav-left">
-              <Link to="/" className="nav-item not-signup">Write a Review</Link>
-              <Link to="/" className="nav-item not-signup">Events</Link>
-              <Link to="/" className="nav-item not-signup">Talk</Link>
+            <div className="splash-top-nav">
+              <div className="splash-top-nav-left">
+                <Link to="/" className="nav-item not-signup">Write a Review</Link>
+                <Link to="/" className="nav-item not-signup">Events</Link>
+                <Link to="/" className="nav-item not-signup">Talk</Link>
+              </div>
+
+              <div className="splash-top-nav-right">
+                <Link to="/login" className="nav-item not-signup login">Log In</Link>
+                <Link to="/signup" className="nav-item sign-up-button">Sign Up</Link>
+              </div>
             </div>
 
-            <div className="splash-top-nav-right">
-              <Link to="/login" className="nav-item not-signup login">Log In</Link>
-              <Link to="/signup" className="nav-item sign-up-button">Sign Up</Link>
+            <div className="splash-header-logo">
+              <img src='https://i.imgur.com/RZ5UvrT.png' />
             </div>
+
+            <div className="splash-search-container">
+
+              <div className="splash-search-input left-bar">
+                <span>Find</span>
+                <input type="text"
+                  onChange={this.handleChange()}
+                  placeholder="labradors, pugs, poodles..."
+                  className="left-input"
+                />
+              </div>
+
+              <div className="splash-search-input right-bar">
+                <span>Near</span>
+                <input type="text"
+                  onChange={this.handleChange()}
+                  placeholder="San Francisco, CA"
+                  className="right-input"
+                />
+              </div>
+
+              <div className="splash-search-button">
+                <i class="fas fa-search"></i>
+              </div>
+            </div>
+
+
+
           </div>
-
-          <div className="splash-header-logo">
-            <img src='https://i.imgur.com/RZ5UvrT.png' />
-          </div>
-
-          <div className="splash-search-container">
-
-            <div className="splash-search-input left-bar">
-              <span>Find</span>
-              <input type="text"
-                onChange={this.handleChange()}
-                placeholder="labradors, pugs, poodles..."
-                className="left-input"
-              />
-            </div>
-
-            <div className="splash-search-input right-bar">
-              <span>Near</span>
-              <input type="text"
-                onChange={this.handleChange()}
-                placeholder="San Francisco, CA"
-                className="right-input"
-              />
-            </div>
-
-            <div className="splash-search-button">
-              <i class="fas fa-search"></i>
-            </div>
-          </div>
-
-
-
         </div>
-      </div>
     );
   }
 }
