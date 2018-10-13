db4d2579b794038359743238dd722f5baf612e14
Tyler <tylerthomasallen@gmail.com>
2018-10-13 12:11:41 -0700

    Finish css for top half of index page

--- frontend/components/splash/splash_header_item.jsx
+++ frontend/components/splash/splash_header_item.jsx
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 
 class SplashHeaderItem extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   // <div className="splash-login">
   //   <ul className="splash-login-ul">
   //     <li><Link to="/login">Log In</Link></li>
   //     <li><Link to="/signup">Sign Up</Link></li>
   //   </ul>
   // </div>
+
+  handleChange() {
+
+  }
 
   render() {
     return (
@@ -25,10 +35,41 @@ class SplashHeaderItem extends React.Component {
             </div>
 
             <div className="splash-top-nav-right">
-              <Link to="/login" className="nav-item not-signup">Log In</Link>
+              <Link to="/login" className="nav-item not-signup login">Log In</Link>
               <Link to="/signup" className="nav-item sign-up-button">Sign Up</Link>
             </div>
           </div>
+
+          <div className="splash-header-logo">
+            <img src='https://i.imgur.com/RZ5UvrT.png' />
+          </div>
+
+          <div className="splash-search-container">
+
+            <div className="splash-search-input left-bar">
+              <span>Find</span>
+              <input type="text"
+                onChange={this.handleChange()}
+                placeholder="labradors, pugs, poodles..."
+                className="left-input"
+              />
+            </div>
+
+            <div className="splash-search-input right-bar">
+              <span>Near</span>
+              <input type="text"
+                onChange={this.handleChange()}
+                placeholder="San Francisco, CA"
+                className="right-input"
+              />
+            </div>
+
+            <div className="splash-search-button">
+              <i class="fas fa-search"></i>
+            </div>
+          </div>
+
+
 
         </div>
       </div>
