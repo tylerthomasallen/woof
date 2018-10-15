e90e18778c843f0ae11e31cb5739c9dd262e36c0
Tyler <tylerthomasallen@gmail.com>
2018-10-15 13:39:48 -0700

    Add sticky position to more dog info

--- frontend/components/dog/more_dog_info.jsx
+++ frontend/components/dog/more_dog_info.jsx
@@ -9,10 +9,77 @@ class MoreDogInfo extends React.Component {
     return (
       <div className="more-dog-info">
         <h3>More dog info</h3>
+
         <div className="dog-info-attribute">
-          <span>Good With Kids</span>
+          <span>Size</span>
+          <span>{'Large'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Good for Kids</span>
           <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
         </div>
+
+        <div className="dog-info-attribute">
+          <span>Hypoallergenic</span>
+          <span>{dog.hypoallergenic ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Adapts Well to Apartment Living</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Easy to Groom</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Easy to Train</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Good for Novice Owners</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Affectionate with Family</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Dog Friendly</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Energy Level</span>
+          <span>{'High'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Barks</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Tolerates Cold Weather</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Tolerates Warm Weather</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
+        <div className="dog-info-attribute">
+          <span>Tolerates Warm Weather</span>
+          <span>{dog.good_with_kids ? 'Yes' : 'No'}</span>
+        </div>
+
       </div>
     );
   }
