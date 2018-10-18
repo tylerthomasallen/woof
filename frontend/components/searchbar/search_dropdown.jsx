a5e7a9b8456437f13fafb43ce6b315af1dd0784a
Tyler <tylerthomasallen@gmail.com>
2018-10-18 10:59:42 -0700

    Finish styling for search dropdown menu

--- frontend/components/searchbar/search_dropdown.jsx
+++ frontend/components/searchbar/search_dropdown.jsx
@@ -21,7 +21,38 @@ class SearchDropdown extends React.Component {
     return (
       <div className={`${formType}-search-dropdown-container`}
         id={`${side}-search-bar-dropdown`}>
-        <span>HEllo</span>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Hound</span>
+        </div>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Herding</span>
+        </div>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Sporting</span>
+        </div>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Terrier</span>
+        </div>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Toy</span>
+        </div>
+
+        <div className={`${formType}-search-dropdown-item`}>
+          <i class="fas fa-bone"></i>
+          <span>Working</span>
+        </div>
+
+
       </div>
     );
   }
