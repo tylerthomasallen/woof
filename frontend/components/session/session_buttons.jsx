5a3c8b62507c3a57b49e80bb501367e460761d22
Tyler <tylerthomasallen@gmail.com>
2018-10-31 18:51:17 -0700

    Finish mobile responsive header

--- frontend/components/session/session_buttons.jsx
+++ frontend/components/session/session_buttons.jsx
@@ -11,14 +11,14 @@ class SessionButtons extends React.Component {
     const { currentUser, logout, formType } = this.props;
     if (!currentUser) {
       return (
-        <div className={`splash-top-nav-right`}>
+        <div className={`${formType}-top-nav-right`}>
           <Link to="/login" className={`nav-items not-signup ${formType}-login`}>Log In</Link>
           <Link to="/signup" className={`nav-items ${formType}-sign-up-button`}>Sign Up</Link>
         </div>
       );
     } else {
       return (
-        <div className={`splash-top-nav-right`}>
+        <div className={`${formType}-top-nav-right`}>
           <a className={`nav-items not-signup logout`} onClick={() => logout()}>Log out</a>
         </div>
       );
